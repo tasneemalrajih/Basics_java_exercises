bb6490d5c776e598fc571c4bfa3eb339b603f3cb
tasneemalrajih <86051479+tasneemalrajih@users.noreply.github.com>
2024-06-02 09:14:21 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,24 @@
+public class Course {
+    private  String title;
+    private  double price;
+    public Course(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,32 @@
+import jdk.dynalink.beans.StaticClass;
+
+public class Student {
+
+    String name;
+    private double mark;
+    static int fullMark;
+
+    public Student(String name, double mark){
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public double getPercent(){
+        return mark/fullMark;
+    }
+    public String getGrade(){
+        if(mark>85) return "Excllent";
+        else if (mark>75)return "Very Good";
+        else if (mark>65) return "Good";
+        else if (mark>=50) return "Pass";
+        else return "Fail";
+    }
+    public double getMark(){
+        return mark;
+    }
+    public  void setmark(double mark){
+        if(mark >= 0){
+            this.mark=mark;
+        }
+    }
+}
